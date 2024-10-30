3e9fac08e47e6ccba9a3e7586f3b54300a20ca38
Harsh Kumar <harsh.kumar1858@indifi.com>
2024-10-30 13:09:06 +0530

    Cache check

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,6 @@ MainBP = Blueprint('main', __name__)
 MainBP.register_blueprint(ChildBP, url_prefix='/v1')
 MainBP.add_url_rule('/operation', view_func=OperationView.as_view('operation'))
 
+#hello this is harsh
+
 
